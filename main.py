1e0ab9ea6f9ff81d2d33af352531d94343ad6819
Niqee <niqz99@gmail.com>
2019-12-19 17:20:14 +0200

    Подключение логики к гпи
    Реализована логика для кнопок:
    + Главное окно:
    +-- Шаг
    +-- Сброс
    +-- Новый лабиринт
    + Окно настроек:
    +-- Сгенерировать

--- main.py
+++ main.py
@@ -15,17 +15,71 @@ class ExploreManager(object):
     """
     Класс для описания исследования лабиринта
     """
+    WALL_COLOR = (12, 22, 57)
+    UNEXPLORED_COLOR = (162, 162, 162)
+    PROCRASTINATED_COLOR = (245, 170, 42)
+    EXPLORED_COLOR = (90, 160, 90)
+    BOT_COLOR = (252, 26, 26)
 
     def __init__(self):
         self.maze: Optional[Maze] = None
         self.bot_list: Optional[List[Bot]] = None
         self.task_list: Optional[List[Tuple[int, int]]] = None
         self.running: bool = False
+        plt.ion()
+        self.img = None
+        self.step = 0
 
-    def update_data(self, new_data):
+    def update_data(self, new_data: Tuple[int, int, int]):
+        """
+        Метод для обновления симуляции
+
+        Parameters
+        ----------
+        new_data : tuple
+            Новые данные в формате (new_bot_num, new_height, new_width), где
+                new_bot_num - новое количество ботов
+                new_height - новая вертикаль
+                new_width - новая горизоталь
+        """
         self.setup_simulation(bot_number=new_data[0],
                               height=new_data[1],
                               width=new_data[2])
+        self.step = 0
+        self.show_matrix(hard=True)
+
+    def show_matrix(self, hard: bool = False):
+        """
+        Метод для вывода лабиринта
+
+        Parameters
+        ----------
+        hard : bool
+            Если True то график будет полностью сброшен перед отображением
+        """
+        show_matrix = np.zeros(self.maze.matrix.shape + tuple([3]))
+        for height_idx in range(self.maze.height):
+            for width_idx in range(self.maze.width):
+                if self.maze.matrix[height_idx, width_idx] == Maze.WALL_BLOCK:
+                    show_matrix[height_idx, width_idx, :] = ExploreManager.WALL_COLOR
+                elif self.maze.matrix[height_idx, width_idx] == Maze.UNEXPLORED_BLOCK:
+                    show_matrix[height_idx, width_idx, :] = ExploreManager.UNEXPLORED_COLOR
+                elif self.maze.matrix[height_idx, width_idx] == Maze.EXPLORED_BLOCK:
+                    show_matrix[height_idx, width_idx, :] = ExploreManager.EXPLORED_COLOR
+                elif self.maze.matrix[height_idx, width_idx] == Maze.PROCRASTINATED_BLOCK:
+                    show_matrix[height_idx, width_idx, :] = ExploreManager.PROCRASTINATED_COLOR
+                else:
+                    show_matrix[height_idx, width_idx, :] = ExploreManager.BOT_COLOR
+        if self.img is None or hard:
+            if self.img is not None:
+                plt.close()
+            _, ax = plt.subplots()
+            self.img = ax.imshow(show_matrix.astype(np.uint8))
+
+        else:
+            self.img.set_data(show_matrix.astype(np.uint8))
+        plt.draw()
+        plt.pause(0.01)
 
     def setup_simulation(self,
                          manual_matrix: np.ndarray = None,
@@ -71,6 +125,8 @@ class ExploreManager(object):
 
         self.maze.change_block(starting_point, Maze.BOT_BLOCK)
         self.bot_list[0].activate(starting_point)
+
+        self.step = 1
 
         self.running = True
 
@@ -156,37 +212,77 @@ class ExploreManager(object):
             bot.activate(task)
             self.maze.change_block(task, Maze.BOT_BLOCK)
 
-    def on_step_click(self):
+        self.step += 1
+
+    def get_step(self) -> int:
+        """
+        Метод для получения номера текущего хода
+
+        Returns
+        -------
+        step : int
+            Номер текущего хода
+        """
+        return self.step
+
+    def get_progress(self) -> float:
+        """
+        Метод для получения текущего прогресса
+
+        Returns
+        -------
+        progress : float
+            Текущий прогресс
+        """
+        not_wall_num = np.sum(self.maze.matrix != Maze.WALL_BLOCK)
+        explored_num = np.sum(self.maze.matrix == Maze.EXPLORED_BLOCK)
+        return explored_num / not_wall_num
+
+    def on_step_click(self, win: MainWindow):
+        """
+        Метод, вызывающийся при нажатии на кнопку "Шаг"
+
+        Parameters
+        ----------
+        win : MainWindow
+            Обьект главного окна
+        """
         if self.running:
             self.exploring_step()
+            win.update_step(self.step)
+            win.update_progress(self.get_progress())
+            self.show_matrix()
         else:
             warn('Входные параметры не заданны, выполнение невозможно')
 
-    def on_reset_click(self):
+    def on_reset_click(self, win: MainWindow):
+        """
+        Метод, вызывающийся при нажатии на кнопку "Сброс"
+
+        Parameters
+        ----------
+        win : MainWindow
+            Обьект главного окна
+        """
         if self.maze is not None:
             self.maze.reset()
             self.setup_simulation(manual_matrix=self.maze.matrix)
+            win.update_step(self.step)
+            win.update_progress(self.get_progress())
+            self.show_matrix()
         else:
             warn('Лабиринт не задан, сброс невозможен')
 
 
 if __name__ == '__main__':
     em = ExploreManager()
-    # while len(em.get_active_bots()) > 0:
-    #     em.exploring_step()
-    #     plt.imshow(em.maze.matrix)
-    #     plt.pause(0.01)
-    #     plt.clf()
-    #
-    # plt.imshow(em.maze.matrix)
-    # plt.show()
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow(em)
 
     # window.link_start_btn()
     # window.link_stop_btn()
-    window.link_step_btn(em.on_step_click)
-    window.link_reset_btn(em.on_reset_click)
+    window.link_step_btn(lambda: em.on_step_click(window))
+    window.link_reset_btn(lambda: em.on_reset_click(window))
     window.link_new_maze_btn(window.setup_win.show)
     # window.link_save_btn()
 
